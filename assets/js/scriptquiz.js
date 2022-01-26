0cf0977377dd551161f57a286c296d38fa4d56ac
Matthew Tanner <matthew.j.tanner@gmail.com>
2022-01-26 17:35:33 -0500

    Update scriptquiz.js
    Corrected a typo.

--- assets/js/scriptquiz.js
+++ assets/js/scriptquiz.js
@@ -34,7 +34,7 @@ function decClock() {
 //Poppulates the quiz answer buttons.
 function query(q){ 
   var quizArray = [["Commonly used data types DO NOT include:","1. strings","2. booleans","3. alerts","4. numbers",3],
-                  ["The condition in an if/else statement is enclosed within _____","1. quotes","2. curly brackets","3. parenthesis","4. square brackets",3],
+                  ["The condition in an if/else statement is enclosed within _____","1. quotes","2. curly brackets","3. parentheses","4. square brackets",3],
                   ["Arrays in JavaScript can be used to hold _____.","1. numbers and strings","2. other arrays","3. booleans","4. all of the above",4],
                   ["Strings must be enclosed within _____ when being assigned to a variable.","1. commas","2. curly brackets","3. quotes","4. parenthesis",3],
                   ["A very useful tool used during development and debugging for printed content to the debugger is:","1. JavaScript","2. terminal / bash","3. for loops","4. console.log",4]];
